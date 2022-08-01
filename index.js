9d5253a020fede59752256222fe37b112c1cb548
Phongsakorn Wisetthon <boy1556@hotmail.com>
2022-08-01 08:07:14 +0700

    test

--- index.js
+++ index.js
@@ -22,38 +22,38 @@ let headers = {
 fastify.get('/', async (request, reply) => {
     //let rotateofspeed from query rotatespeed
     let rotatespeed = request.query.rotatespeed
-    if(intnumber == 'xxxxxxxxxx' || intnumber == '' || intnumber.length != 10) {
-        return {result: 'ignore', Msg: 'Please set internet id in environment variable aisfiber_internet_id'}
+    if (intnumber == 'xxxxxxxxxx' || intnumber == '' || intnumber.length != 10) {
+        return { result: 'ignore', Msg: 'Please set internet id in environment variable aisfiber_internet_id' }
     }
     //if lasttime < nowtime 5 minute
     nowtime = new Date();
-    if(lasttime.getTime() + 300000 > nowtime.getTime()) {
-        return {result: 'ignore', Msg: 'please wait 5 minutes for next request'}
-    }else{
-        const getrequest = await fetch("https://myaisfibre.com/index.php", {"headers": headers,"body": "page=toggleSpeed&action=confirmChangeSpeed&data=rotate("+rotatespeed+")","method": "POST"})
+    if (lasttime.getTime() + 300000 > nowtime.getTime() && lasttime.getTime() != nowtime.getTime()) {
+        return { result: 'ignore', Msg: 'please wait 5 minutes for next request' }
+    } else {
+        const getrequest = await fetch("https://myaisfibre.com/index.php", { "headers": headers, "body": "page=toggleSpeed&action=confirmChangeSpeed&data=rotate(" + rotatespeed + ")", "method": "POST" })
         const getresponse = await getrequest.json();
-        if(getresponse.Result == 'OK' || getresponse.result == 'OK') {
-            const confrimrequest = await fetch("https://myaisfibre.com/index.php", {"headers": headers,"body": "page=toggleSpeed&action=toggleBandwidth&data%5Bnon%5D="+intnumber+"&data%5Bcode%5D=rotate("+rotatespeed+")","method": "POST"})
+        if (getresponse.Result == 'OK' || getresponse.result == 'OK') {
+            const confrimrequest = await fetch("https://myaisfibre.com/index.php", { "headers": headers, "body": "page=toggleSpeed&action=toggleBandwidth&data%5Bnon%5D=" + intnumber + "&data%5Bcode%5D=rotate(" + rotatespeed + ")", "method": "POST" })
             const confrimresponse = await confrimrequest.json();
             const result = confrimresponse.Result || confrimresponse.result;
-            if(result.toUpperCase() == 'SUCCESS') {
+            if (result.toUpperCase() == 'SUCCESS') {
                 lasttime = new Date();
-                return {result: 'success', Msg: 'change internet speed to'+confrimresponse.data}
-            }else{
-                return {result: 'error', Msg: confrimresponse.detail}
+                return { result: 'success', Msg: 'change internet speed to' + confrimresponse.data }
+            } else {
+                return { result: 'error', Msg: confrimresponse.detail }
             }
-        }else{
-            return {result: 'error', Msg: getresponse.Msg}
+        } else {
+            return { result: 'error', Msg: getresponse.Msg }
         }
     }
 })
 
 const start = async () => {
     try {
-      await fastify.listen({ port: 3000, host: '0.0.0.0' })
+        await fastify.listen({ port: 3000, host: '0.0.0.0' })
     } catch (err) {
-      fastify.log.error(err)
-      process.exit(1)
+        fastify.log.error(err)
+        process.exit(1)
     }
 }
 
