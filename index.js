a42506cf4f97f83cb139f6f292b11d34cb524d28
Phongsakorn Wisetthon <boy1556@hotmail.com>
2022-08-01 20:13:26 +0700

    Msg

--- index.js
+++ index.js
@@ -56,6 +56,7 @@ fastify.get('/', async (request, reply) => {
         }
         const getrequest = await fetch("https://myaisfibre.com/index.php", { "headers": headers, "body": "page=toggleSpeed&action=confirmChangeSpeed&data=rotate(" + rotatespeed + ")", "method": "POST" })
         const getresponse = await getrequest.json();
+        console.log(getresponse.Msg);
         if ((getresponse.Result == 'OK' || getresponse.result == 'OK') && getresponse.Msg != speed) {
             const confrimrequest = await fetch("https://myaisfibre.com/index.php", { "headers": headers, "body": "page=toggleSpeed&action=toggleBandwidth&data%5Bnon%5D=" + intnumber + "&data%5Bcode%5D=rotate(" + rotatespeed + ")", "method": "POST" })
             const confrimresponse = await confrimrequest.json();
